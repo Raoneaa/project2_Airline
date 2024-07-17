e578596fbd97bcec56139ee45a007046f0dc2e55
Raoneaa <js2004539@gmail.com>
2024-07-16 22:00:02 -0500

    added all comments to what the functions do

--- main.c
+++ main.c
@@ -1,37 +1,48 @@
-
+/*
+this project was written by Julian solis on July 10, 2024
+it takes in a csv file from the command line then reads the data
+it then allows the user to search the data using origin and destinations
+then outputing the data found
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "route-records.h"
 
 int main(int argc, char *argv[]) {
+//tell user to input file if 2 arguemnts are not read
     if (argc != 2) {
-        printf("Usage: %s <input_file>\n", argv[0]);
+        printf("Please input file into command line when starting program");
         return 1;
     }
-
+// create file pointer and open file from the command line using argv[1]
     FILE *fileIn = fopen(argv[1], "r");
+// if it count open the file given in the comand line then give error message
     if (!fileIn) {
         fprintf(stderr, "Error: Could not open file %s\n", argv[1]);
         return 1;
     }
-
+// creating pointer for records by calling the createRecords function
     RouteRecord* records = createRecords(fileIn);
+// fill in the records from read data and assign recordCount int to what is returned from function
     int recordCount = fillRecords(records, fileIn);
 
     printf("Unique routes operated by airlines: %d\n", recordCount);
-
+    fclose(fileIn);
     int choice;
+// initializing the keys and setting their max size including the null terminator
     char key1[4], key2[4];
-
+// infinite loop that repeats until the user inputs 5 to exit
     do {
+// calls the print menu
         printMenu();
+
         if (scanf("%d", &choice) != 1) {
             fprintf(stderr, "Invalid input\n");
             while (getchar() != '\n'); // Clear the buffer
             continue;
         }
-
+// switcase statement comparing the choice from the user
         switch (choice) {
             case 1:
                 printf("Enter origin airport: ");
@@ -58,13 +69,13 @@ int main(int argc, char *argv[]) {
             case 5:
                 printf("Goodbye!\n");
                 break;
+// if any input that is not the case then it gives an error message then scans again
             default:
                 printf("Invalid choice. Please try again.\n");
                 break;
         }
     } while (choice != 5);
-
+// frees the database
     free(records);
-    fclose(fileIn);
     return 0;
 }
--- route-records.c
+++ route-records.c
@@ -3,26 +3,30 @@
 #include <stdio.h>
 #include "route-records.h"
 
-RouteRecord* createRecords( FILE* fileIn ) {
+RouteRecord* createRecords(
+ FILE* fileIn )
+ {
+// count is the number of lines in the file
     int count = 0;
     char buffer[300];
-
+// read the number of lines updating count to reflect the lines read
     while(fgets(buffer,sizeof(buffer),fileIn)){
         count++;
     }
     count --;
-
+// initialize the array using the number of lines(count) * the size of the strict RouteRecord
     RouteRecord* records = (RouteRecord*)malloc(count*sizeof(RouteRecord));
     if(!records) {
         printf("Error allocating memory\n");
         return NULL;
     }
-
+// set all passengers to 0
     for(int i = 0; i < count; ++i) {
         for(int j = 0; j < 6; ++j) {
             records[i].passengers[j] = 0;
         }
     }
+//rewind to the top of the file and return the record pointer
     rewind(fileIn);
     return records;
 }
@@ -35,12 +39,16 @@ int findAirlineRoute(
             const char* airline,
             int index )
 {
+// basecase, if it doesnt find anthing before getting to negative index then return -1 to
+// give no found message
     if(index >= length) {
         return -1;
     }
+// if found the return the index that its at
     if(strcmp(r[index].origin,origin) == 0 && strcmp(r[index].destination,destination) == 0 && strcmp(r[index].airline,airline) == 0) {
         return index;
     }
+// recurse if its not found
     return findAirlineRoute(r,length,origin,destination,airline,index +1);
 }
 
@@ -49,43 +57,56 @@ int fillRecords(
     RouteRecord* r,
     FILE* fileIn )
 {
-
+// initialize the variables
     char buffer[300];
     char origin[4];
     char destination[4];
     char airline[3];
     int passengers,month;
     int records = 0;
-
+// skip the header line
     fgets(buffer,sizeof(buffer),fileIn);
 
-
+// read the file and input the data from the buffer until it reaches the end of the file
     while(fgets(buffer, sizeof(buffer),fileIn)) {
         sscanf(buffer,"%d,%3s,%3s,%2s,,%d",&month,origin,destination,airline,&passengers);
+// after scanning in it doesnt assign it untit verified that that route doesnt already exists
         int  index =  findAirlineRoute(r,records,origin,destination,airline,0);
         // if -1 then there is already a route with data, input passengers into that route
         if(index != -1) {
             r[index].passengers[month-1] = passengers;
         }
+// if no route is found then it makes a new record for that route
         else {
             strcpy(r[records].origin, origin);
             strcpy(r[records].destination,destination);
             strcpy(r[records].airline,airline);
             r[records].passengers[month -1] = passengers;
+// update the record count by 1
             ++records;
         }
     }
+// return the number of records
     return records;
 }
 
-void searchRecords(RouteRecord* r, int length, const char* key1, const char* key2, SearchType st) {
+void searchRecords(
+        RouteRecord* r,
+        int length,
+        const char* key1,
+        const char* key2,
+        SearchType st)
+     {
+// initialize the variables that are needed to see if there is a match
     int totalPassengers = 0;
     int monthPassengers[6] = {0};
     int matches = 0;
-
+// loop throught the whole thing using length and comparing to the sent data
     for (int i = 0; i < length; i++) {
+// match is used as a true false statement
         int match = 0;
-
+// switch case to check what searchtype is being done, then compares to see if there is anthing that
+// matches the search type and keys
         switch (st) {
             case ROUTE:
                 if (strcmp(r[i].origin, key1) == 0 && strcmp(r[i].destination, key2) == 0) {
@@ -111,7 +132,7 @@ void searchRecords(RouteRecord* r, int length, const char* key1, const char* key
                 match = 0;
                 break;
         }
-
+// if  there is a match it adds 1 to the number of mathces and prints out the route
         if (match) {
             matches++;
 
@@ -124,9 +145,10 @@ void searchRecords(RouteRecord* r, int length, const char* key1, const char* key
         }
     }
     printf("\n");
+    // assigning total passengers to the whatever is in the records and index of each
     totalPassengers += r->passengers[0] + r->passengers[1] + r->passengers[2] +
                        r->passengers[3] + r->passengers[4] + r->passengers[5];
-
+// if matches is greater then 0, meaning some where found, then it outputs that data
     if (matches > 0) {
         printf("%d matches were found.\n", matches);
         printf("Statistics\n");
